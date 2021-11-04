db6eca8495bcb22bf75a7acdc90cedba9958cef6
DevBengel <72806734+DevBengel@users.noreply.github.com>
2021-11-04 20:14:01 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,7 +15,7 @@ include accounts::groups
     managehome  => true,
     gid         => 'pipelineusers',
     groups      => "$rootgroup",
-     password    => '',
+    password    => 'YiZW0yqiJLwK.',
   }
 
 }
